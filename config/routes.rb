1faed91a4f84b09f706d8a88e51a4e4e071c2838
Nguyễn Anh Tuấn <tuannguyenk60@gmail.com>
2018-07-20 12:48:02 +0700

    user log in, log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   
   get "sign_up", to: "users#new"
   post "sign_up", to: "users#create"
+  get "log_in", to: "sessions#new"
+  post "log_in", to: "sessions#create"
+  delete "log_out", to: "sessions#destroy"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -129,7 +129,7 @@ ActiveRecord::Schema.define(version: 2018_07_19_064345) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.boolean "admin", default: false
+    t.integer "role", default: 0
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
