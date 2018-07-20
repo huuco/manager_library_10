2cc86111f3906d1ff60e1f2adcb4d8a57d366a0f
Nguyễn Anh Tuấn <tuannguyenk60@gmail.com>
2018-07-20 08:28:39 +0700

    User sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,11 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent: :destroy
 
-  before_save {email.downcase!}
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX}
+            
+  before_save {email.downcase!}
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  
+  get "sign_up", to: "users#new"
+  post "sign_up", to: "users#create"
 end
