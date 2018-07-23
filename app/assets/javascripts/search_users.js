473c8e57a173d370d51a23e6ae0e85d7502f71f2
Nguyễn Anh Tuấn <tuannguyenk60@gmail.com>
2018-07-23 22:04:03 +0700

    Export list user to csv, excel

--- app/assets/javascripts/search_users.js
+++ app/assets/javascripts/search_users.js
@@ -1,22 +1,43 @@
 $(document).ready(function() {
-  $("#_all").click(function() {
+  $("#_all").click(function(e) {
+    e.preventDefault();
     $("#_role").val("");
-    $("#search-users").submit();
+    search("", $("#_search").val());
+    $("#_all").addClass("btn-primary");
     return false;
   });
-  $("#_users").click(function() {
+  $("#_users").click(function(e) {
+    e.preventDefault();
     $("#_role").val(0);
-    $("#search-users").submit();
+    search(0, $("#_search").val());
+    $("#_users").addClass("btn-primary");
     return false;
   });
-  $("#_managers").click(function() {
+  $("#_managers").click(function(e) {
+    e.preventDefault();
     $("#_role").val(1);
-    $("#search-users").submit();
+    search(1, $("#_search").val());
+    $("#_managers").addClass("btn-primary");
     return false;
   });
-  $("#_admins").click(function() {
+  $("#_admins").click(function(e) {
+    e.preventDefault();
     $("#_role").val(2);
-    $("#search-users").submit();
+    search(2, $("#_search").val());
+    $("#_admins").addClass("btn-primary");
     return false;
   });
 });
+
+function search(role, search){
+  $("form .btn").removeClass("btn-primary");
+  $.ajax({
+    type: "GET",
+    url: "/admin",
+    data: {role: role, search: search},
+    dataType: 'script',
+  });
+  $("#export-csv").attr("href", "/admin/users.csv?utf8=%E2%9C%93&search=" + search + "&role=" + role);
+  $("#export-xls").attr("href", "/admin/users.xls?utf8=%E2%9C%93&search=" + search + "&role=" + role);
+}
+
