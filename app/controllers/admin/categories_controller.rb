680b2af6149eec40977fe95753fff12cdb73be42
Nguyễn Anh Tuấn <tuannguyenk60@gmail.com>
2018-07-24 10:18:01 +0700

    Admin create, edit, delete category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,66 @@
 class Admin::CategoriesController < AdminController
+  before_action :get_category, only: [:edit, :update, :destroy]
+
   def index
-    @categories = if params[:search].present?
-      Category.select_categories.get_by_title params[:search]
-    else
-      Category.select_categories
+    @categories = Category.select_categories.get_by_title(params[:search])
+                          .order(:title).includes(:books)
+                          .page(params[:page]).per Settings.pages.per_ctg
+    respond_to do |format|
+      format.html
+      format.js
     end
-    @categories = @categories.order(:title)
-                             .page(params[:page]).per Settings.pages.per_ctg
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t ".add_success" + " " + @category.title
+      redirect_to admin_categories_path
+    else
+      flash[:error] = t ".add_fail"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t ".update_success"
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    unless @category.books.any?
+      if @category.destroy
+        flash[:success] = t ".delete_success"
+      else
+        flash[:danger] = t ".delete_fail"
+      end
+    else
+      flash[:danger] = t ".ctg_has_books"
+    end
+    redirect_to admin_categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title, :describe)
+  end
+
+  def get_category
+    @category = Category.find_by id: params[:id]
+    return if @category
+      flash[:danger] = t "not_found" + params[:id]
+      redirect_to admin_categories_path
   end
 end
