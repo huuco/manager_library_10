b05d4f7722006f9afb54a6aef081f3b8b8d6ca54
Nguyễn Anh Tuấn <tuannguyenk60@gmail.com>
2018-08-29 11:00:03 +0700

    social share button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,7 @@
 //= require bootstrap
 //= require Chart.bundle
 //= require chartkick
+//= require social-share-button
 //= require_tree .
 
 $(document).ready(function(){
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: "tuannguyenk60@gmail.com"
+  layout "mailer"
 end
